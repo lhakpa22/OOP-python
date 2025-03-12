d33fcda246aa8dcf5056794c7925b7faf04618a5
lhakpa22 <149383977+lhakpa22@users.noreply.github.com>
2025-03-12 19:22:36 +0000

    feat(user): class added

--- #OOP Python.py
+++ #OOP Python.py
@@ -1,2 +1,10 @@
 #OOP Python
+# User Class (Parent)
+class User:
+    def __init__(self, name, user_id, password, email):
+        self.name = name
+        self._user_id = user_id  # Protected attribute
+        self.__password = password  # Private attribute
+        self.email = email
+
 
