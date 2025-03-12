4a673774eee75a2435e26cf9886fc87231c2cd64
lhakpa22 <149383977+lhakpa22@users.noreply.github.com>
2025-03-12 19:41:50 +0000

    updating

--- #OOP Python.py
+++ #OOP Python.py
@@ -7,6 +7,15 @@ class User:
         self.__password = password  # Private attribute
         self.email = email
 
+    # getter for password
+    def get_passwors(self):
+        return self.__password
+
+    # setter for password
+    def set_password(self, new_password):
+        self.__password = new_password
+        print("password updated.")
+
     def login(self):
         print(f"{self.name} logged in.")
 
